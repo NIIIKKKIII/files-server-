e4e251ec6c51b964672c6a19b097c00bcf091c29
Nikhil Bhati <bhatinikhil709@gmail.com>
2024-07-18 15:34:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.get('/files', function(req, res){
+    fs.readdir(path.join(__dirname), (err, data)=>{
+        if(err){
+            return res.json("cannot file the fucking file")
+        }else{
+            res.send(data)
+        }
+    })
+})
+
+
+
+app.get('/file/:filename', function(req,res){
+const filepath = path.join(__dirname, req.params.filename);
+fs.readFile(filepath, "utf-8", (err, data)=>{
+    if(err){
+       return res.send(err, "error found ")
+    }else{
+      return  res.send(data)
+    }
+})
+})
+
+
+
+
+
+
+
+
+app.listen(3000);
